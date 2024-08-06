bfe54a10e33ad6ad884c66c1ef922ddb58499b57
Zartex <zartexshow@gmail.com>
2024-08-06 10:46:57 +0200

    Auto commitau

--- .github/workflows/Auto.sh
+++ .github/workflows/Auto.sh
@@ -1,8 +1,8 @@
 while true; do
   inotifywait -r -e modify,attrib,close_write,move,create,delete ./
   git add .
-  git commit -m "Auto commit"
+  git commit -m "Auto commit"au
   git push origin main
-inotifywait -r -e modify,attrib,close_write,move,create,delete ./
+  inotifywait -r -e modify,attrib,close_write,move,create,delete ./
   git pull origin main
 done
