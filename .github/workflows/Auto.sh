da713044bc6901699036e4609d4e114e00f72548
Zartex <zartexshow@gmail.com>
2024-08-06 10:43:51 +0200

    Auto commit

---
+++ .github/workflows/Auto.sh
@@ -0,0 +1,8 @@
+while true; do
+  inotifywait -r -e modify,attrib,close_write,move,create,delete ./
+  git add .
+  git commit -m "Auto commit"
+  git push origin main
+inotifywait -r -e modify,attrib,close_write,move,create,delete ./
+  git pull origin main
+done
